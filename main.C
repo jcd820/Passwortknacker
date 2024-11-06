77d2cef101d91a4cd0b38c90233b048663208338
sschlecht98 <samuel.schlecht@hsbi.de>
2024-11-06 16:56:33 +0100

    Kommentar

---
+++ main.C
@@ -0,0 +1,34 @@
+//============================================================================
+// Name        : Gruppe6.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <string>
+#include <iostream>
+
+#include <cstdio>      /* printf, NULL */
+#include <cstdlib>     /* srand, rand */
+#include <ctime>       /* time */
+
+#include <unistd.h>
+
+#include "SHA256.H"
+#include "TASK1.H"
+
+
+using namespace std;
+
+
+int main(){
+	srand(time(nullptr));
+
+    return 0;
+}
+
+
+
+
+
